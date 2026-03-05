b75aa3384c4ad1baab3d788d1029e361367e3c7b
junoandmihyun <draw.round@gmail.com>
2026-03-05 17:21:38 +0900

    clean up fade in effect

--- script.js
+++ script.js
@@ -147,24 +147,3 @@ function copyAccountSix() {
   // Alert the copied text
   alert("복사되었습니다. " + copyText.value);
 }
-
-// Fade in Effect
-document.addEventListener("DOMContentLoaded", function () {
-
-  const sections = document.querySelectorAll(".section");
-
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-      }
-    });
-  }, {
-    threshold: 0.2
-  });
-
-  sections.forEach(section => {
-    observer.observe(section);
-  });
-
-});
